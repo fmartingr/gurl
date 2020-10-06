b2f90f453d7ae4cdd99ccb38f4b7becf6ef3cbb9
Felipe M <me@fmartingr.com>
2020-10-06 17:10:43 +0200

    Allow body, headers and verb in request

--- main.go
+++ main.go
@@ -19,30 +19,57 @@ const (
 	userAgent = "Gurl/0.0.1a1"
 )
 
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return "my string representation"
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
 func main() {
-	displayRequest := flag.Bool("request", false, "Display request")
-	displayResponse := flag.Bool("response", true, "Display response")
+	var extraHeaders arrayFlags
+	flag.Var(&extraHeaders, "header", "Extra header to append to request (repatable)")
+	displayRequest := flag.Bool("request", false, "Display request in output")
+	displayResponse := flag.Bool("response", true, "Display response in output")
+	verb := flag.String("verb", "GET", "Verb to use on the request")
+	body := flag.String("body", "", "Body to send with request")
 	flag.Parse()
 
 	if len(flag.Args()) == 0 {
 		log.Print("Missing URL to connect to")
+		flag.Usage()
 		os.Exit(cli.ErrorNoUrl)
 	}
 	uri := tokenizer.Tokenize(flag.Args()[0])
 
 	conn, err := net.Dial("tcp", uri.DialAddress())
 	if err != nil {
-		log.Print("Error connecting:", err.Error())
+		log.Print("Error connecting to ", uri.Repr())
+		log.Print(err.Error())
 		os.Exit(cli.ErrorConnectionOpen)
 	}
 
+	headers := []http.Header{
+		{Name: "Host", Value: uri.Hostname},
+		{Name: "User-Agent", Value: userAgent},
+		{Name: "Connection", Value: "close"},
+	}
+
+	for _, h := range extraHeaders {
+		header := strings.SplitN(h, ":", 2)
+		headers = append(headers, http.Header{Name: header[0], Value: header[1]})
+	}
+
 	outgoingRequest := http.NewRequest(
+		strings.ToUpper(*verb),
 		uri.Path,
-		[]http.Header{
-			{Name: "Host", Value: uri.Hostname},
-			{Name: "User-Agent", Value: userAgent},
-			{Name: "Connection", Value: "close"},
-		})
+		headers,
+		*body,
+	)
 	if *displayRequest {
 		fmt.Println(outgoingRequest.Repr())
 	}
--- pkg/http/request.go
+++ pkg/http/request.go
@@ -1,5 +1,9 @@
 package http
 
+import (
+	"strconv"
+)
+
 // Header interpretation for a request
 type Header struct {
 	Name  string
@@ -12,16 +16,27 @@ type Request struct {
 	Path     string
 	Protocol string
 	Headers  []Header
+	Body     string
 }
 
 // NewRequest Creates a new request
-func NewRequest(path string, headers []Header) Request {
-	return Request{
-		Verb:     "GET",
+func NewRequest(verb string, path string, headers []Header, body string) Request {
+	request := Request{
+		Verb:     verb,
 		Path:     path,
 		Protocol: "HTTP/1.1",
 		Headers:  headers,
+		Body:     body,
 	}
+
+	// If request contains a body, append the content-length header
+	if request.Body != "" {
+		request.Headers = append(
+			request.Headers,
+			Header{Name: "Content-Length", Value: strconv.Itoa(len(request.Body))})
+	}
+
+	return request
 }
 
 // Repr Return the string represntation of the request
@@ -30,5 +45,5 @@ func (r Request) Repr() string {
 	for _, h := range r.Headers {
 		result += h.Name + ": " + h.Value + "\r\n"
 	}
-	return result + "\r\n"
+	return result + "\r\n" + r.Body + "\r\n\r\n"
 }
